c9b389f104d2f8d6505370c143369bd551d8308a
bramvano <bramvano@8914e18f-62a5-2a26-923c-8c88b3d75a6a>
2010-04-14 21:10:48 +0000

    - Added the system namespace - Added the Files namespace

--- trunk/trunk/library/xbmc/jsonrpc.class.js
+++ trunk/trunk/library/xbmc/jsonrpc.class.js
@@ -1,38 +1,38 @@
 function JsonRpc (Xbmc)
 {
-    var s_namespace     = 'JSONRPC';
+    var s_namespace = 'JSONRPC';
 
     this.introspect = function ()
     {
         var o_result = Xbmc.post(s_namespace, 'Introspect');
-        return (o_result)? o_result.commands : false ;
+        return (o_result && o_result.commands != undefined)? o_result.commands : false ;
     }
 
     this.version = function ()
     {
         var o_result = Xbmc.post(s_namespace, 'Version');
-        return (o_result)? o_result.version : false ;
+        return (o_result && o_result.version != undefined)? o_result.version : false ;
     }
 
     this.permission = function ()
     {
         var o_result = Xbmc.post(s_namespace, 'Permission');
-        return (o_result)? o_result.permission : false ;
+        return (o_result && o_result.permission != undefined)? o_result.permission : false ;
     }
 
     this.ping = function ()
     {
         var o_result = Xbmc.post(s_namespace, 'Ping');
-        return (o_result == "pong")? true : false ;
+        return (o_result && o_result == "pong")? true : false ;
     }
 
     this.announce = function (s_sender, s_message)
     {
-        var a_parameters        = new Object();
-        a_parameters.sender     = s_sender;
-        a_parameters.message    = s_message;
-        var o_result            = Xbmc.post(s_namespace, 'Announce', a_parameters);
+        var o_parameters        = new Object();
+        o_parameters.sender     = s_sender;
+        o_parameters.message    = s_message;
+        var o_result            = Xbmc.post(s_namespace, 'Announce', o_parameters);
 
-        return (o_result == "OK")? true : false ;
+        return (o_result && o_result == "OK")? true : false ;
     }
 }
