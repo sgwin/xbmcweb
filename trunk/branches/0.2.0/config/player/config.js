3f40fac9663662c0b82a384d922e06bde21430da
bramvano <bramvano@8914e18f-62a5-2a26-923c-8c88b3d75a6a>
2010-04-26 06:28:17 +0000

    Added a Status namespace to optimize perfomance of status updates (still nog optimal)

--- trunk/branches/0.2.0/config/player/config.js
+++ trunk/branches/0.2.0/config/player/config.js
@@ -2,20 +2,22 @@ var config      = new Array();
 config['xbmc']  = new Array();
 config['app']   = new Array();
 
-config['xbmc']['address']   = "localhost";
-config['xbmc']['port']      = "8080";
-config['xbmc']['username']  = "xbmc";
-config['xbmc']['password']  = "xbmc";
-config['xbmc']['debug']     = false;
+config['xbmc']['address']       = "localhost";
+config['xbmc']['port']          = "8080";
+config['xbmc']['username']      = "xbmc";
+config['xbmc']['password']      = "xbmc";
+config['xbmc']['debug']         = false;
 
-config['app']['name']       = 'player';
-config['app']['style_path'] = 'style/application/' +config['app']['name']+ '/';
+config['app']['name']           = 'player';
+config['app']['upate_interval'] = 2; //seconds
+config['app']['style_path']     = 'style/application/' +config['app']['name']+ '/';
 
 
 
 //Do not edit below this line\\
-var Xbmc            = new Xbmc(config['xbmc']);
-var a_modules       = new Array();
+var Xbmc        = new Xbmc(config['xbmc']);
+var a_modules   = new Array();
+var o_statusUpdateInterval;
 
 a_modules[0]        = 'now_playing';
 a_modules[1]        = 'media_browser';
@@ -25,11 +27,40 @@ a_modules[4]        = 'status';
 
 $(document).ready(function ()
 {
+    $('body').layout(
+    { 
+        applyDefaultStyles:     true,
+        west__closable:         true,
+        west__resizable:        true,
+        west__size:		400,
+        north__closable:        false,
+        north__resizable:       false,
+        north__size:            140,
+        center__closable:       false,
+        center__resizable:      true,
+        south__closable:        false,
+        south__resizable:       false,
+        south__size:            65
+    });
+
+
+    clearInterval(o_statusUpdateInterval);
+    Xbmc.Status.update();
+
     if (!Xbmc.JsonRpc.ping())
         alert("Could not connect to XBMC.\nMake sure the webservice is enabled.");
     else
     {
         for (var x=0; x<a_modules.length; x++)
             $('#' +a_modules[x]+ '_container').load('view/' +config['app']['name']+ '/'+a_modules[x]+'.html');
+
+        o_statusUpdateInterval = setInterval
+        (
+            function ()
+            {
+                Xbmc.Status.update();
+            },
+            (config['app']['upate_interval']*1000)
+        );
     }
 });
